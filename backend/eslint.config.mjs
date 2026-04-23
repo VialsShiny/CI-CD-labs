650889811f526c7188e37ffa00e5a22db2f22344
VialsShiny <vialsvialatou@gmail.com>
2026-04-23 14:10:40 +0200

    Try Fix eslint/jest config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,5 +1,5 @@
 import js from '@eslint/js';
-import jestPlugin from 'eslint-plugin-jest';
+import jest from 'eslint-plugin-jest';
 import globals from 'globals';
 
 export default [
@@ -14,32 +14,8 @@ export default [
 
     {
         files: ['**/*.{test,spec}.{js,mjs,cjs}'],
-        plugins: {
-            jest: jestPlugin,
-        },
-        rules: {
-            'jest/valid-expect': 'error',
-            'jest/valid-expect-location': 'error',
-            'jest/expect-expect': [
-                'error',
-                {assertFunctionNames: ['expect', 'expect*']},
-            ],
-            'jest/no-conditional-expect': 'error',
-            'jest/no-standalone-expect': 'error',
-            'jest/no-alias-methods': 'error',
-            'jest/no-mocks-import': 'error',
-            'jest/prefer-todo': 'error',
-            'jest/prefer-to-be-null-descriptor': 'error',
-            'jest/prefer-to-have-length': 'error',
-            'jest/prefer-called-with': 'error',
-            'jest/prefer-equality-matcher': 'error',
-
-            'jest/no-disabled-tests': 'warn',
-            'jest/no-focused-tests': 'error',
-            'jest/no-identical-title': 'error',
-            'jest/no-jest-import': 'warn',
-            'jest/no-test-return-statement': 'error',
-        },
+        plugins: {jest},
+        ...jest.configs.recommended,
         languageOptions: {
             globals: {
                 ...globals.jest,
